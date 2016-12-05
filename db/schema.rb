0691192f9a971e03db430ec5509f3ede92a040e5
Matthew Bunday <zenceph@gmail.com>
2016-12-05 13:23:51 -0500

    Add authentication but leave it off for just now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205175139) do
+ActiveRecord::Schema.define(version: 20161205181938) do
 
   create_table "nodes", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161205175139) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "api_key"
   end
 
 end
