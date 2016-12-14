60525ce3a086c22802e3391f5cbcbe168e381eb7
Matthew Bunday <zenceph@gmail.com>
2016-12-14 02:36:19 -0500

    Switch over to using postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161205181938) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "nodes", force: :cascade do |t|
     t.string   "name"
     t.string   "content"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Node.create(content: 'ILUVU')
+Node.create(content: 'Ninja')
+Node.create(content: 'Magick')
+Node.create(content: 'Wizard')
