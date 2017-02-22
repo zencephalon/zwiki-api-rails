369ecf77663744b38feb747ae5085d3ee9c2cb5d
Matthew Bunday <zenceph@gmail.com>
2017-02-22 13:34:01 -0500

    Basic static generator

--- bin/generate_static.rb
+++ bin/generate_static.rb
@@ -1,4 +1,7 @@
 require File.expand_path('../../config/environment', __FILE__)
+require 'github/markup'
+
+LINK_REGEX = /\[([^\[]+)\]\(([^)]+)\)/
 
 def template(name, content)
 <<HTML
@@ -10,7 +13,9 @@ def template(name, content)
     <body>
       <div id="root"></div>
       <script src="/static/bundle.js"></script>
+      <article>
 #{content}
+      </article>
     </body>
   </html>
 HTML
@@ -18,7 +23,13 @@ end
 
 Node.all.each do |node|
   filename = "#{node.id}.html"
+  content = node.content.gsub(LINK_REGEX) do |match|
+    p match
+    p $1
+    title = Node.find_by(id: $2).name.parameterize
+    "[#{$1}](#{$2}/#{title})"
+  end
   File.open(filename, 'w') do |f|
-    f.puts template(node.name, node.content)
+    f.puts template(node.name, GitHub::Markup.render('foo.markdown', content))
   end
 end
