3a6d2a7095caeb39d7907a555809f52f5f98c2c7
Matthew Bunday <zenceph@gmail.com>
2017-02-22 15:41:36 -0500

    Better styling and js

--- gen/zwik.js
+++ gen/zwik.js
@@ -4,6 +4,12 @@ var ID = function () {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
 
+var markAsVisited = function(href) {
+  var currentUrl = window.location.href;
+  history.replaceState({}, '', href);
+  history.replaceState({}, '', currentUrl);
+}
+
 var handleLinkCount = function($article, inc) {
   var count = $article.data('count') || 0;
   count += inc;
@@ -17,13 +23,30 @@ var handleLinkCount = function($article, inc) {
   $article.data('count', count);
 }
 
+var handleLinkClick = function($link) {
+  if (!$link.data('id')) {
+    var id = ID();
+    var href = $link.attr('href');
+    $link.after('<article id="' + id + '" >' + doc_cache[href] + '</article>');
+    $link.data('id', id);
+    $link.addClass('open');
+    handleLinkCount($link.closest('article'), 1);
+    markAsVisited(href);
+  } else {
+    var id = $link.data('id');
+    $('#' + id).remove();
+    $link.data('id', '');
+    $link.removeClass('open');
+    handleLinkCount($link.closest('article'), -1);
+  }
+}
+
 $(document).ready(function() {
   $('body').on('mouseover', 'a', function() {
     var $link = $(this);
-    var href = $link.attr('href') + '.txt';
+    var href = $link.attr('href');
     if (!doc_cache[href]) {
-      $.get(href).done(function(data) {
-        console.log('got data', data);
+      $.get(href + '.txt').done(function(data) {
         doc_cache[href] = data;
       })
     }
@@ -31,18 +54,17 @@ $(document).ready(function() {
 
   $('body').on('click', 'a', function(e) {
     var $link = $(this);
-    if (!$link.data('id')) {
-      var id = ID();
-      var href = $link.attr('href') + '.txt';
-      $link.after('<article id="' + id + '" >' + doc_cache[href] + '</article>');
-      $link.data('id', id);
-      handleLinkCount($link.closest('article'), 1);
-    } else {
-      var id = $link.data('id');
-      $('#' + id).remove();
-      $link.data('id', '');
-      handleLinkCount($link.closest('article'), -1);
-    }
+    handleLinkClick($link);
     e.preventDefault();
+    e.stopPropagation();
   })
+
+  // close all links in the article
+  $('body').on('click', 'article', function(e) {
+    $(this).find('a.open').each(function() {
+      handleLinkClick($(this));
+    });
+    e.stopPropagation();
+  })
+
 })
