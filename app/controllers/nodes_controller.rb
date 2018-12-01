eabee4198153a0a3fece24112f81a9e5cda83ade
Matthew Bunday <zenceph@gmail.com>
2018-11-30 22:16:26 -0500

    Better access control on node controller

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -49,7 +49,7 @@ class NodesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_node
-      @node = Node.find_by(short_id: params[:id])
+      @node = @current_user.nodes.find_by(short_id: params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
