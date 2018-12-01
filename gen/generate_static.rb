470290a48547df849279142e27665113fb342b5a
Matthew Bunday <zenceph@gmail.com>
2018-12-01 01:08:48 -0500

    Clean up generate static

--- gen/generate_static.rb
+++ gen/generate_static.rb
@@ -1,13 +1,3 @@
 require File.expand_path('../../config/environment', __FILE__)
 
-style_file = File.expand_path('../style.css', __FILE__)
-js_file = File.expand_path('../zwik.js', __FILE__)
-favicon = File.expand_path('../favicon.ico', __FILE__)
-logo = File.expand_path('../zenchinese.png', __FILE__)
-
-`cp #{style_file} export/`
-`cp #{js_file} export/`
-`cp #{favicon} export/`
-`cp #{logo} export/`
-
 User.find_by(name: 'zen_public').export_nodes
