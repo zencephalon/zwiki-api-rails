f4d80da4e77e2245837657d103d356ecbafb819d
Matthew Bunday <zenceph@gmail.com>
2019-09-04 22:34:03 -0400

    Finish crazy upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :nodes
+  resources :users do
+    get 'me', to: 'users#me', on: :collection
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'login', to: 'sessions#login'
+  post 'register', to: 'sessions#register'
 end
