99552d02a69ed94b40bdd6fbc1940c91617db47f
Matthew Bunday <mkbunday@gmail.com>
2021-09-20 16:21:05 -0400

    Fix CORS for vercel

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'zwiki.zencephalon.com'
+     origins 'zwiki.zencephalon.com', 'zwiki-client-react.vercel.app'
 
      resource '*',
        headers: :any,
