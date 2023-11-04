27a278827d42f4416179444b658fd4732de1209a
Matthew Bunday <mkbunday@gmail.com>
2023-11-04 13:47:16 -0400

    Update quest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :quests
+  # resources :quests
   put 'quests', to: 'quests#update'
   get 'quests', to: 'quests#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
