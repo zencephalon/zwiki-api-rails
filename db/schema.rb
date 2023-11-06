ff5be32bee921b0db32b4f557484a35fffcbb071
Matthew Bunday <mkbunday@gmail.com>
2023-11-06 18:04:31 -0500

    Correctly set defaults

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_06_224500) do
+ActiveRecord::Schema.define(version: 2023_11_06_230309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2023_11_06_224500) do
     t.json "blob"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "version"
+    t.integer "version", default: 0
     t.index ["user_id"], name: "index_quests_on_user_id"
   end
 
