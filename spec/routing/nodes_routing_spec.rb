044a19747d377d1622daf5973c28c0fcb40f05e0
Matthew Bunday <mkbunday@gmail.com>
2026-01-07 22:22:11 -0500

    Remove test spam

--- spec/routing/nodes_routing_spec.rb
+++ spec/routing/nodes_routing_spec.rb
@@ -7,16 +7,8 @@ RSpec.describe NodesController, type: :routing do
       expect(:get => "/nodes").to route_to("nodes#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/nodes/new").to route_to("nodes#new")
-    end
-
     it "routes to #show" do
       expect(:get => "/nodes/1").to route_to("nodes#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/nodes/1/edit").to route_to("nodes#edit", :id => "1")
     end
 
     it "routes to #create" do
